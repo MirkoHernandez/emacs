68175ddd1c8ee7cccecbeae8cc33a201a96d6053
math <math@math-host.math-domain>
2018-09-30 01:50:04 -0300

    Change outline-regexp  for c-mode.

--- lisp/c-config.el
+++ lisp/c-config.el
@@ -14,20 +14,20 @@
              (outline-minor-mode)
              (hs-minor-mode)
 	     (flycheck-mode)
+	     (smartparens-mode)
 	     ;; Kernel Coding Style
 	     (setq c-basic-offset 8)
 	     (setq c-default-style "k&r"
 		   c-basic-offset 8
 		   company-backends '(company-gtags company-c-headers))
-             (setq outline-regexp "\\(@\\* \\|@\\*\\* \\|@\\*\\*\\* \\|@ \\| @ \\)" )))
-
+             (setq outline-regexp "///" )))
 
 (defun my-autoindent-c ()
+  ""
   (interactive)
   (when (executable-find "indent")
     (shell-command (concat "indent -kr -cli0 -cbi0 -ss -i8 -ip8 -ppi 2 "
 			   (buffer-file-name) ))))
-	  
 
 ;;@============================= FONTIFICATION
 (font-lock-add-keywords 'c-mode
