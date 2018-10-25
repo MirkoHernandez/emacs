d91f3a2ac873405dc554a5a8c2d6e0dbf0221f00
Mirko Hernandez <mirkoh@fastmail.com>
2018-10-25 01:10:38 -0300

    Add keybinding for indent defun.

--- lisp/keybinding.el
+++ lisp/keybinding.el
@@ -57,6 +57,7 @@
 ;;@============================= GENERAL KEYBINDINGS
 (global-set-key (kbd "C--") 'text-scale-decrease)
 (global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "<C-tab>") 'c-indent-defun)
 
 (global-set-key "\C-xp" 'fill-paragraph)
 (global-set-key [f7] 'next-error)
