8e8a61e3ee1dcec16c09f1806f1a50f18a7abf71
Mirko Hernandez <mirkoh@fastmail.com>
2018-12-12 21:03:16 -0500

    Add melpa to the list of package archives (required for racket-mode).

--- lisp/packages-setup.el
+++ lisp/packages-setup.el
@@ -12,6 +12,9 @@
 (package-initialize)
 
 ;;@============================= PACKAGE LIST
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/")
+             t)
 
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
