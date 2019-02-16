1aed47ea1513b6bf3583d0957957d11643879284
h <h@h.h-d>
2019-02-16 11:05:44 -0500

    Modify indentation settings for c-mode (increase line-length to 185).

--- lisp/c-config.el
+++ lisp/c-config.el
@@ -26,7 +26,7 @@
   ""
   (interactive)
   (when (executable-find "indent")
-    (shell-command (concat "indent -kr -cli0 -cbi0 -ss -i8 -ip8 -ppi 2 "
+    (shell-command (concat "indent -kr -cli0 -cbi0 -ss -i8 -ip8 -ppi 2 --line-length185 "
 			   (buffer-file-name) ))))
 
 ;;@============================= FONTIFICATION
