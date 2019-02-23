420294ebf5d024dba67400bd157c460bbfe2c76c
Mirko Hernandez <mirkoh@fastmail.com>
2019-02-23 10:41:56 -0500

    Include function find-corresponding-file.

--- lisp/efuncs.el
+++ lisp/efuncs.el
@@ -9,6 +9,55 @@
   (split-window-right)
   (other-window 1))
 
+
+(defun find-corresponding-file ()
+  "Find the file that corresponds to this one."
+  (interactive)
+  (find-file
+   (let ((base-filename (file-name-sans-extension (buffer-name)))
+	 (file-name (buffer-name)))
+     (cond
+      ;; Cpp config
+      ((string-match "\\.c$" file-name)
+       (concat base-filename ".h"))
+      ((string-match "\\.cpp$" file-name)
+       (concat base-filename ".h"))
+      ((string-match "\\.h$" file-name)
+       (if (file-exists-p (concat base-filename ".c"))
+	   (concat base-filename ".c")
+	 (concat base-filename ".cpp")))
+      ;; Fractal template to config.
+      ((string-match "\\.hbs$\\|\\.mustache$\\|\\.twig$" file-name)
+       (cond
+	((file-exists-p (concat base-filename ".config.json"))
+	 (concat base-filename ".config.json"))
+	((file-exists-p (concat base-filename ".config.js"))
+	 (concat base-filename ".config.js"))
+	((file-exists-p (concat base-filename ".config.yaml"))
+	 (concat base-filename ".config.yaml"))
+	((file-exists-p (concat base-filename ".config.yml"))
+	 (concat base-filename ".config.yml"))
+	(t (error "Unable to find a corresponding file"))))
+      ;; Fractal config to template.
+      ((string-match "\\.js$\\|\\.json$\\|\\.yaml$\\|\\.yml$" file-name)
+       (let ((base-filename (car (split-string base-filename ".config"))))
+       (cond
+	((file-exists-p (concat base-filename ".hbs"))
+	 (concat base-filename  ".hbs"))
+	((file-exists-p (concat base-filename ".mustache"))
+	 (concat base-filename  ".mustache"))
+	((file-exists-p (concat base-filename ".twig"))
+	 (concat  base-filename ".twig"))
+	(t (error "Unable to find a corresponding file")))))
+      (t (error "Unable to find a corresponding file"))))))  
+
+(defun find-corresponding-file-other-window ()
+  "Find the file that corresponds to this one."
+  (interactive)
+  (find-file-other-window buffer-file-name)
+  (casey-find-corresponding-file)
+  (other-window -1))
+
 ;;@============================= EDITING
 (defun indent-or-complete ()
   "Complete if point is at end of a word, otherwise indent line."
@@ -80,4 +129,3 @@ to the next line."
 (defun thunar()
   (interactive)
   (start-process "thunar" nil "thunar" (buffer-file-name) ))
-
