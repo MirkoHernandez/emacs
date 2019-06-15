c20cda0f7a7a791be289d2306990387ea053cea4
Mirko Hernandez <mirkoh@fastmail.com>
2019-06-15 10:16:00 -0400

    Modify compilation commands, implement them using hash table.

--- lisp/c-compilation-config.el
+++ lisp/c-compilation-config.el
@@ -1,12 +1,10 @@
 ;;@============================= LIBRARIES
 
-(defconst OPENGL " -IGL -IGLUT -lglut -lGLEW -lGL " )
 
 (defconst HANDMADE_COMPILER_FLAGS20  " -MT -nologo -Gm- -GR- -EHa- -Zo -O2 -Oi  -W4 -wd4201 -wd4100 -wd4189 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7 -Fmwin32_handmade.map ")
 (defconst HANDMADE_LINKER_FLAGS20 " -opt:ref user32.lib gdi32.lib winmm.lib ")
 
-
-(defconst HANDMADE_COMPILER_FLAGS30  "      -MTd -nologo -fp:fast -Gm- -GR- -EHa- -Od -Oi  -W4 -wd4201 -wd4100 -wd4189 -wd4505 -wd4127 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7 ")
+(defconst HANDMADE_COMPILER_FLAGS30  " -MTd -nologo -fp:fast -Gm- -GR- -EHa- -Od -Oi  -W4 -wd4201 -wd4100 -wd4189 -wd4505 -wd4127 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7 ")
 ;; Maximize Speed
 (defconst HANDMADE_COMPILER_FLAGS119  " -O2 -MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -Oi  -W4 -wd4201 -wd4100 -wd4189 -wd4505 -wd4127 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7 ")
 ;; No optimizations
@@ -14,22 +12,57 @@
 
 (defconst HANDMADE_LINKER_FLAGS143 " -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib ")
 
+(setq handmade-flags
+      #s(hash-table size 20 test equal
+		    data (
+			  ;; Linker Flags
+			  "linker20" " -opt:ref user32.lib gdi32.lib winmm.lib "
+			  "linker143" " -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib "
+			  ;; Compiler Flags
+			  "compiler20" " -MT -nologo -Gm- -GR- -EHa- -Zo -O2 -Oi  -W4 -wd4201 -wd4100 -wd4189 
+                                         -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7 -Fmwin32_handmade.map "
+			  "compiler30" " -MTd -nologo -fp:fast -Gm- -GR- -EHa- -Od -Oi  -W4 -wd4201 -wd4100 -wd4189 
+                                         -wd4505 -wd4127 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7 "
+			  ;; Maximize speed
+			  "compiler119" " -O2 -MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -Oi  -W4 -wd4201 
+                                          -wd4100 -wd4189 -wd4505 -wd4127 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 
+                                          -DHANDMADE_WIN32=1 -FC -Z7 "
+			  ;; No optimizations
+			  "compiler143"  "-Od  -MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -Oi  -W4 -wd4201 
+                                          -wd4100 -wd4189 -wd4505 -wd4127 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 
+                                          -DHANDMADE_WIN32=1 -FC -Z7 "
+			   "dll144"  " /LD /link -incremental:no -opt:ref -PDB:handmade_%random%.pdb -EXPORT:GameGetSoundSamples -EXPORT:GameUpdateAndRender"
+			   )))
+
+(setq handmade-table
+      #s(hash-table size 20 test equal
+		    data (
+			  "handmade20" (lambda (filename) (concat "cl "  (gethash "compiler20" handmade-flags) filename
+					       " /link -subsystem:windows,5.1 "  (gethash "compiler20" handmade-flags))))
+		    ))
+
 (defun compile-handmade20(filename)
-  (concat "cl "  HANDMADE_COMPILER_FLAGS20 filename " /link -subsystem:windows,5.1 "  HANDMADE_LINKER_FLAGS20 ))
+  (concat "cl "  HANDMADE_COMPILER_FLAGS20 "../win32_handmade.cpp
+ /link -subsystem:windows,5.1 "  HANDMADE_LINKER_FLAGS20 ))
+
 
 (defun compile-handmade30 (filename)
   (concat
    "del *.pdb > NUL 2> NUL && "
-   "cl " HANDMADE_COMPILER_FLAGS30 " ../handmade.cpp -Fmhandmade.map /LD /link -incremental:no -opt:ref -PDB:handmade_%random%.pdb -EXPORT:GameGetSoundSamples -EXPORT:GameUpdateAndRender "  
-          " && cl " HANDMADE_COMPILER_FLAGS30 " ../win32_handmade.cpp  -Fmhandmade.map  /link  "   HANDMADE_LINKER_FLAGS143 ))
-
+   "cl " HANDMADE_COMPILER_FLAGS30
+   " ../handmade.cpp -Fmhandmade.map /LD /link -incremental:no -opt:ref -PDB:handmade_%random%.pdb -EXPORT:GameGetSoundSamples -EXPORT:GameUpdateAndRender  && "
+   "cl "  HANDMADE_COMPILER_FLAGS30
+   " ../win32_handmade.cpp  -Fmhandmade.map  /link  "
+   HANDMADE_LINKER_FLAGS143 ))
 
 (defun compile-handmade119 (filename)
   (concat
    "del *.pdb > NUL 2> NUL && "
-   "cl " HANDMADE_COMPILER_FLAGS119 " ../handmade.cpp -Fmhandmade.map /LD /link -incremental:no -opt:ref -PDB:handmade_%random%.pdb -EXPORT:GameGetSoundSamples -EXPORT:GameUpdateAndRender "  
-          " && cl " HANDMADE_COMPILER_FLAGS119 " ../win32_handmade.cpp  -Fmhandmade.map  /link  "   HANDMADE_LINKER_FLAGS143 ))
-
+   "cl " HANDMADE_COMPILER_FLAGS119
+   " ../handmade.cpp -Fmhandmade.map /LD /link -incremental:no -opt:ref -PDB:handmade_%random%.pdb 
+     -EXPORT:GameGetSoundSamples -EXPORT:GameUpdateAndRender  && "
+   "cl " HANDMADE_COMPILER_FLAGS119 " ../win32_handmade.cpp  -Fmhandmade.map  /link  "
+   HANDMADE_LINKER_FLAGS143 ))
 
 (defun compile-handmade143 (filename)
   (concat
@@ -39,30 +72,40 @@
 
 (defun compile-handmade144 (filename)
   (concat
-   "del *.pdb > NUL 2> NUL && "
-   "cl " HANDMADE_COMPILER_FLAGS143 " -O2 -I../iaca-win64/ -c ../handmade_optimized.cpp -Fohandmade_optimized.obj  -LD "  
+   "del *.pdb > NUL 2> NUL && "   "cl " HANDMADE_COMPILER_FLAGS143 " -O2 -I../iaca-win64/ -c ../handmade_optimized.cpp -Fohandmade_optimized.obj  -LD "  
    " && cl " HANDMADE_COMPILER_FLAGS143 " -I../iaca-win64/   ../win32_handmade.cpp handmade_optimized.obj -Fmhandmade.map -LD  /link -incremental:no -opt:ref -PDB:handmade_%random%.pdb -EXPORT:GameGetSoundSamples -EXPORT:GameUpdateAndRender "
    " && del lock.tmp  "
    " && cl " HANDMADE_COMPILER_FLAGS143 " ../win32_handmade.cpp  -Fmwin32_handmade.map  /link  "   HANDMADE_LINKER_FLAGS143 ))
 
 
 (if (eq system-type 'windows-nt)
-    (progn
-      (defconst SDL2 " -lSDL2_image -lpng -lz -lm")
-      (defconst HANDMADE "")
-      (defconst SDL " -lmingw32 -lSDLmain -lSDL -lSDL_image -lSDL_ttf -lSDL_mixer ")
-      (defconst ALLEGRO " -lallegro-4.4.2-mt"))
-  (progn
-    (defconst SDL2 " `sdl2-config --cflags --libs` -lSDL2_image -lpng -lz")
-    (defconst SDL " `sdl-config --cflags --libs` -lSDL_image -lSDL_ttf -lSDL_mixer ")
-    (defconst ALLEGRO  " `allegro-config --libs` -lm  -lpng -lz"))) ;;  -lm  -lpng -lz -laldmd -ldumbd ldumb"
+    (setq libraries-table
+      #s(hash-table size 20 test equal
+		    data (
+			  "sdl2" "-lSDL2_image -lpng -lz -lm"
+			  "sdl"  "-lmingw32 -lSDLmain -lSDL -lSDL_image -lSDL_ttf -lSDL_mixer"
+			  "winlibs" "user32.lib gdi32.lib winmm.lib"
+			  "opengl"   "-IGL -IGLUT -lglut -lGLEW -lGL"
+			  "allegro"  "-lallegro-4.4.2-mt")))
+    (setq libraries-table
+      #s(hash-table size 20 test equal
+		    data (
+			   "sdl2"     "`sdl2-config --cflags --libs` -lSDL2_image -lpng -lz"
+			   "sdl"      "`sdl-config --cflags --libs` -lSDL_image -lSDL_ttf -lSDL_mixer"
+			   "dumb"     "-laldmd -ldumbd -ldumb"
+			   "srgp"     "-L/usr/X11R6/lib -lsrgp -lX11"
+			   "opengl"   "-IGL -IGLUT -lglut -lGLEW -lGL"
+			   "allegro5" "`allegro-config --libs` `pkg-config --cflags --libs allegro-5.0  ` -lldpng  -lpng -lz"
+			   "allegro"  "`allegro-config --libs` -lm  -lpng -lz" ))))
 
-
-(defconst ALLEGRO5" `allegro-config --libs` `pkg-config --cflags --libs allegro-5.0  ` -lldpng  -lpng -lz ")
-(defconst SRGP " -L/usr/X11R6/lib -lsrgp -lX11] ")
-(defconst WINLIBS " user32.lib gdi32.lib winmm.lib ")
-
-(defconst terminal-run  "gnome-terminal -x ")
+(defun libraries-string (l)
+  (let ((libraries-string " ")
+	(libraries (split-string l)))
+    (mapc (lambda (x)
+	    (when (gethash x libraries-table)
+	       (setq libraries-string (concat libraries-string (gethash x libraries-table) " ")))
+	    ) libraries)
+    libraries-string))
 
 ;;@============================= HELPERS
 (defun buffer-name-no-extension ()
@@ -89,89 +132,66 @@
       (let ((default-directory (concat default-directory "build")))
         (compile compile-command)))))
 
-
 ;;@============================= GENERATING COMPILE STRINGS
-(defun libraries-string (l)
-  (let ((libraries "" ))
-    (cond
-     ((member 'sdl l) (setq libraries (concat libraries  SDL)))
-     ((member 'sdl2 l) (setq libraries (concat libraries  SDL2)))
-     ((member 'opengl l) (setq libraries (concat libraries  OPENGL)))
-     ((member 'windows l) (setq libraries (concat libraries  WINLIBS)))
-     ((member 'allegro l) (setq libraries (concat libraries  ALLEGRO))))
-    libraries))
+(setq c-compile-table
+      #s(hash-table
+	 size 20
+	 test equal
+	 data (
+	       "vs" "cl -Zi "
+	        "cweb" "something"
+	       "cweb cpp"  (lamda (buffername) ) (concat "ctangle ../" buffername ".w_cpp && " )
+	       "cweb c"  (lamda (buffername) ) (concat "ctangle ../" buffername ".w && " ))))
 
 (defun create-compile-string(buffername options)
-  (let  ((compile-string "")
-	 (c (member 'c options))
-	 (cpp (member 'cpp options))
-	 (cweb (member 'cweb options))
-	 (debug (member 'debug options))
-	 (make (member 'make options))
-	 (handmade  (member 'handmade options))
-	 (multiple (member 'multiple options))
-	 (profile (member 'profile options))
-	 (run (member 'run options))
-	 (vs (member 'windows  options)))
-    (cond
-     (make
-      (setq compile-string "make"))
-     (handmade
-      (setq compile-string (compile-handmade144 (concat " ../win32_handmade.cpp" ))))
-
-     
-     ((or c cpp)
-      (let  ((file-extension (if c ".c " ".cpp "))
-	     (cweb-extension (if c ".w" ".w_cpp "))
-	     (command-string (if c  (if vs "cl -Zi " "gcc -std=c99 ")
-			       (if vs "cl -Zi " " g++ "))))
-	(progn (when cweb  
-		 (setq compile-string (concat "ctangle ../"
-					      buffername
-					      cweb-extension
-					      " && ")))
-	       (setq compile-string (concat compile-string
-					    command-string))
-	       (when profile
-		 (setq compile-string (concat
-				       compile-string
-				       "-pg ")))
-	       (when debug
-		 (setq compile-string (concat
-				       compile-string
-				       "-g ")))
-	       (if multiple
-		   (setq compile-string (concat
-					 compile-string
-					 "  ../*"
-					 file-extension
-					 " -o main"))
-		 (setq compile-string (concat compile-string
-					      "../"
-					      buffername
-					      file-extension
-					      " -o "
-					      buffername )))
-	       (setq compile-string (concat
-				     compile-string
-				     (libraries-string options)
-				     " "))
-	       (when run
-		 (setq compile-string (concat compile-string
-					      " && " (if vs (concat default-directory "build/")
-						       (concat  default-directory "build/"))
-					      buffername)))))))
-    compile-string))
-
-;;(create-compile-string "buffername" '(c ))
-
+  (defun compare-options (rgx)
+    (string-match-p rgx options))
+  (let  ((compile-string ""))
+    (progn
+     (when (compare-options "make")
+       (setq compile-string "make"))
+     (when (string-match-p "handmade[0-9]+" options)
+       (setq compile-string (compile-handmade144 (concat " ../win32_handmade.cpp" ))))
+     (when (string-match-p "cweb" options)
+       (progn
+	 (setq compile-string (concat "ctangle ../"  buffername))
+	 (when (string-match-p "\\<c\\>" options)
+		(setq compile-string (concat compile-string ".w")))
+	 (when (string-match-p "cpp" options)
+	   (setq compile-string (concat compile-string ".w_cpp")))))
+     (cond ((string-match-p "\\<vs\\>" options)
+	    (setq compile-string (concat compile-string "cl -Zi")))
+	   ((string-match-p "\\<c\\>" options)
+	    (setq compile-string (concat compile-string "gcc -std=c99 ")))
+	   ((string-match-p "cpp" options)
+	    (setq compile-string (concat compile-string "g++  "))))
+     (when (string-match-p "profile" options )
+       (setq compile-string (concat compile-string "-pg ")))
+     (when (string-match-p "debug" options )
+       (setq compile-string (concat compile-string "-g ")))
+     (when (string-match "\\<c\\>" options)
+       (if (string-match "multiple" options)
+	   (setq compile-string (concat  compile-string  " ../*"    ".c -o main"))
+	 (setq compile-string (concat compile-string   " ../" buffername ".c  -o " buffername ))))
+     (when (string-match "cpp" options)
+       (if (string-match "multiple" options)
+	   (setq compile-string (concat  compile-string  " ../*"    ".cpp -o main"))
+	 (setq compile-string (concat compile-string   " ../" buffername ".cpp  -o " buffername ))))
+     (when (or (string-match "\\<c\\>" options)
+	       (string-match "cpp" options))
+       (setq compile-string (concat  compile-string   (libraries-string options) " ")))
+     (when (string-match "\\<run\\>" options)
+       (setq compile-string (concat compile-string    " &&  cd .. &&  "  "build/" buffername)))
+     compile-string)))
+ 
 
 ;;@============================= SET COMPILATION COMMAND
 (defun set-compile-command(arg config)
   "Sets compile-command by selecting among a list of options."
-  (interactive "P\nsCompile command: c, cpp, cweb, multiple, debug,profile, windows, libraries (sdl sdl2 allegro opengl): " ) 
+  (interactive "P\nsOptions:  make| cweb | c cpp cweb |  multiple | debug profile | libraries (sdl sdl2 allegro opengl): " ) 
     (set (make-local-variable 'compile-command)
-         (create-compile-string (buffer-name-no-extension) (mapcar (lambda (a) (intern a)) (split-string config))))) 
+         (create-compile-string (buffer-name-no-extension)  config))) 
+         ;; (create-compile-string (buffer-name-no-extension) (mapcar (lambda (a) (intern a)) (split-string config))))) 
 
 ;;@=============================  CHICKEN
 (defun chicken-compile ()
@@ -182,6 +202,7 @@
          (let ((file  (file-name-base)))
            (concat
             "csc " file ".scm ")))))
+
 (defun cweb-terminal ()
   (interactive)
   (unless (or (file-exists-p "makefile")
@@ -258,13 +279,11 @@
              (set (make-local-variable 'compile-command)
 	     (create-compile-string
 	      (buffer-name-no-extension)
-	      (mapcar (lambda (a) (intern a)) (split-string
-					       (concat "c " SDL)))))))
-(add-hook 'c++-mode-hook
+	      "c sdl run " ))))
+
+(add-hook 'c++-mode-hook 
           (lambda ()
              (set (make-local-variable 'compile-command)
 	     (create-compile-string
 	      (buffer-name-no-extension)
-	      (mapcar (lambda (a) (intern a)) (split-string
-					       (concat "cpp windows
-          run " )))))))
+	      "cpp vs run " ))))
