e34e19584a44ff32be495fb96ecc7c8e0f4c7631
h <h@h.h-d>
2019-06-15 10:22:37 -0400

    Include GDB configuration.

--- lisp/c-compilation-config.el
+++ lisp/c-compilation-config.el
@@ -269,7 +269,55 @@
       (if (get-buffer "*Async Shell Command*") ; If old compile window exists
           (progn
             (delete-windows-on (get-buffer "*Async Shell Command*")) ; Delete the compilation window
-            (kill-buffer "*Async Shell Command*") )))))
+            (kill-buffer "*Async Shell Command*"))))))
+
+
+;;@============================= GUD
+(setq  gdb-many-windows t)
+(setq  gdb-show-main nil)
+
+(defadvice gdb-setup-windows (around setup-more-gdb-windows activate)
+  ad-do-it
+  (other-window 1)
+  (gdb-set-window-buffer
+    (gdb-get-buffer-create 'gdb-disassembly-position)))
+
+(defun gdb-setup-windows ()
+  "Layout the window pattern for option `gdb-many-windows'."
+  (gdb-get-buffer-create 'gdb-locals-buffer)
+  (gdb-get-buffer-create 'gdb-stack-buffer)
+  (gdb-get-buffer-create 'gdb-breakpoints-buffer)
+  (set-window-dedicated-p (selected-window) nil)
+  (switch-to-buffer gud-comint-buffer)
+  (delete-other-windows)
+  (let ((win0 (selected-window))
+        (win1 (split-window nil ( / ( * (window-height) 3) 4)))
+        (win2 (split-window nil ( / (window-height) 3)))
+        (win3 (split-window-right)))
+    (gdb-set-window-buffer (gdb-locals-buffer-name) nil win3)
+    (select-window win2)
+    (set-window-buffer
+     win2
+     (if gud-last-last-frame
+         (gud-find-file (car gud-last-last-frame))
+       (if gdb-main-file
+           (gud-find-file gdb-main-file)
+         ;; Put buffer list in window if we
+         ;; can't find a source file.
+         (list-buffers-noselect))))
+    (setq gdb-source-window (selected-window))
+    (let ((win4 (split-window-right)))
+      (gdb-set-window-buffer
+       (gdb-get-buffer-create 'gdb-inferior-io) nil win4))
+    (select-window win1)
+    (gdb-set-window-buffer (gdb-stack-buffer-name))
+    (let ((win5 (split-window-right)))
+      (gdb-set-window-buffer (if gdb-show-threads-by-default
+                                 (gdb-threads-buffer-name)
+                               (gdb-breakpoints-buffer-name))
+                             nil win5))
+    (select-window win0)))
+
 
 
 ;;@============================= HOOKS
