816fe6305ba7a4927049ef470e3c0f8ed7ed7101
Mirko Hernandez <mirkoh@fastmail.com>
2020-02-20 19:47:57 -0300

    Modify ivy configuration(regex and line height).

--- lisp/modes-config.el
+++ lisp/modes-config.el
@@ -22,10 +22,13 @@
 ;;@============================= IVY
 (ivy-mode)
 
-
 (setq ivy-use-virtual-buffers t) 
 (setq enable-recursive-minibuffers t)
 (setq ivy-count-format "(%d/%d) ")
+(setq ivy-height 15)
+;; No regex 
+(setq ivy-initial-inputs-alist nil)
+
 
 (ivy-set-actions
  'counsel-find-file
