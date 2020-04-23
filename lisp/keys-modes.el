fef4928550a75e4380ba36cad527a891072e93ae
mirko <mirkoh@fastmail.com>
2020-04-23 02:53:21 -0400

    Reorganize keys

--- lisp/keys-modes.el
+++ lisp/keys-modes.el
@@ -4,13 +4,45 @@
                            (define-key ivy-minibuffer-map  (kbd "C-o") 'dabbrev-expand)))
 
 ;;@============================= HYDRA
+(global-set-key (kbd "M-r") 'hydra-replace/body)
 (global-set-key (kbd "<f2>") 'hydra-bookmarks/body)
-
+(global-set-key (kbd "C-i") 'hydra-navigation/body)
+(global-set-key (kbd "C-x SPC") 'hydra-rectangle/body)
 
 ;;@============================= HELM
 (global-set-key (kbd "M-i") 'helm-imenu)
 (global-set-key (kbd "M-b") 'helm-buffers-list)
 
+;;@============================= NEOTREE
+(global-set-key (kbd "C-h") 'neotree)
+(add-hook 'neotree-mode-hook
+          (lambda ()
+            (define-key neotree-mode-map (kbd "C-h") 'neotree-hide)))
+
+;;@============================= ORG
+(add-hook 'org-mode-hook (lambda ()
+			   (define-key org-mode-map (kbd "C-c e") 'org-texinfo-export-to-info)
+			   (define-key org-mode-map (kbd "C-c b") (lambda ()  (interactive)
+								    (org-emphasize ?\*)))))
+
+;;@============================= DIRED
+;;(define-key dired-mode-map (kbd "C-q") 'dired-toggle-read-only)
+
+;;@============================= COMPANY MODE 
+
+;; (global-set-key (kbd "C-c /") 'company-files)        ; Force complete file names on "C-c /" key
+;; (define-key company-active-map (kbd "C-p") 'company-select-previous-or-abort)
+;; (define-key company-active-map (kbd "C-P") 'company-select-previous-or-abort)
+;; (define-key company-active-map (kbd "C-n") 'company-select-next-or-abort)
+;; (define-key company-active-map (kbd "C-N") 'company-select-next-or-abort)
+;; (define-key html-mode-map (kbd "C-'") 'company-web-html)
+
+;;@============================= PAREDIT
+(add-hook 'paredit-mode-hook (lambda ()
+			       (define-key paredit-mode-map  (kbd "C-<right>") nil)
+			       (define-key paredit-mode-map  (kbd "C-<left>") nil)
+			       (define-key paredit-mode-map (kbd "C-S-<right>") 'paredit-forward-slurp-sexp)
+			       (define-key paredit-mode-map (kbd "C-S-<left>") 'paredit-forward-barf-sexp)))
 ;;@============================= MULTIPLE  CURSORS:
 ;; Rebind emacs Defaults
 (global-set-key (kbd "C-S-o") 'mc/mark-next-like-this)
@@ -36,6 +68,8 @@
 ;;@============================= IEDIT
 ;; (define-key global-map (kbd "C-;") 'iedit-mode)
 
+
+
 ;;@============================= SMARTSCAN
 (define-key global-map (kbd "M-p") 'smartscan-symbol-go-backward)
 (define-key global-map (kbd "M-n") 'smartscan-symbol-go-forward)
@@ -48,13 +82,28 @@
 ;;(define-key ac-menu-map (kbd "<S-tab>") nil)
 ;;(define-key ac-complete-mode-map (kbd "<tab>") 'yas-next-field)
 
+;;@============================= GO
+(add-hook 'c-mode-common-hook (lambda ()
+                                (define-key c-mode-base-map  (kbd "C-<backspace>") 'backward-kill-word-or-selection)))
 
-;;@============================= NEOTREE
-(global-set-key (kbd "C-h") 'neotree)
-(add-hook 'neotree-mode-hook
-          (lambda ()
-            (define-key neotree-mode-map (kbd "C-h") 'neotree-hide)))
+;;@============================= JAVASCRIPT
+(add-hook 'js2-mode-hook (lambda ()
+                           (define-key js2-mode-map  (kbd "C-x C-e") 'js-send-last-sexp)))
+;;@============================= PYTHON
+(add-hook 'python-mode-hook (lambda ()
+                           (define-key python-mode-map  (kbd "C-x C-e") 'python-shell-send-defun)))
+;;@============================= C
 
+(add-hook 'c-mode-common-hook (lambda ()
+				(define-key c-mode-base-map  (kbd "C-<tab>") 'c-indent-defun)
+				(define-key c-mode-base-map  (kbd "C-i") 'hydra-navigation/body)
+				(define-key c-mode-base-map  (kbd "C-j") 'newline)
+				(define-key c-mode-base-map  (kbd "M-/") 'c-mark-function)
+				(define-key c-mode-base-map  (kbd "C-.") 'xref-find-references)
+				(define-key c-mode-base-map  (kbd "<C-return>") 'electric-newline-and-maybe-indent)
+                                (define-key c-mode-base-map  (kbd "C-<backspace>") 'backward-kill-word-or-selection)))
+;; (add-hook 'compilation-mode-hook (lambda ()
+;; (define-key compilation-mode-map  "q" 'kill-buffer-and-window )))
 
 ;;@============================= RACKET
 (add-hook 'racket-mode-hook
@@ -82,49 +131,3 @@
   (local-set-key [M-down] 'outline-forward-same-level)
   (local-set-key [M-left] 'hide-subtree)
   (local-set-key [C-S-backspace] 'delete)  (local-set-key [M-right] 'show-subtree))
-;;@============================= ORG
-(add-hook 'org-mode-hook (lambda ()
-			   (define-key org-mode-map (kbd "C-c e") 'org-texinfo-export-to-info)
-			   (define-key org-mode-map (kbd "C-c b") (lambda ()  (interactive)
-								    (org-emphasize ?\*)))))
-
-;;@============================= JAVASCRIPT
-(add-hook 'js2-mode-hook (lambda ()
-                           (define-key js2-mode-map  (kbd "C-x C-e") 'js-send-last-sexp)))
-;;@============================= PYTHON
-(add-hook 'python-mode-hook (lambda ()
-                           (define-key python-mode-map  (kbd "C-x C-e") 'python-shell-send-defun)))
-;;@============================= C-MODE
-
-(add-hook 'c-mode-common-hook (lambda ()
-				(define-key c-mode-base-map  (kbd "C-<tab>") 'c-indent-defun)
-				(define-key c-mode-base-map  (kbd "C-j") 'newline)
-				(define-key c-mode-base-map  (kbd "M-/") 'c-mark-function)
-				(define-key c-mode-base-map  (kbd "C-.") 'xref-find-references)
-				(define-key c-mode-base-map  (kbd "<C-return>") 'electric-newline-and-maybe-indent)
-                                (define-key c-mode-base-map  (kbd "C-<backspace>") 'backward-kill-word-or-selection)))
-;; (add-hook 'compilation-mode-hook (lambda ()
-				;; (define-key compilation-mode-map  "q" 'kill-buffer-and-window )))
-
-;;@============================= GO-MODE
-(add-hook 'c-mode-common-hook (lambda ()
-                                (define-key c-mode-base-map  (kbd "C-<backspace>") 'backward-kill-word-or-selection)))
-
-;;@============================= DIRED
-;;(define-key dired-mode-map (kbd "C-q") 'dired-toggle-read-only)
-
-;;@============================= COMPANY MODE 
-
-;; (global-set-key (kbd "C-c /") 'company-files)        ; Force complete file names on "C-c /" key
-;; (define-key company-active-map (kbd "C-p") 'company-select-previous-or-abort)
-;; (define-key company-active-map (kbd "C-P") 'company-select-previous-or-abort)
-;; (define-key company-active-map (kbd "C-n") 'company-select-next-or-abort)
-;; (define-key company-active-map (kbd "C-N") 'company-select-next-or-abort)
-;; (define-key html-mode-map (kbd "C-'") 'company-web-html)
-
-;;@============================= PAREDIT
-(add-hook 'paredit-mode-hook (lambda ()
-			       (define-key paredit-mode-map  (kbd "C-<right>") nil)
-			       (define-key paredit-mode-map  (kbd "C-<left>") nil)
-			       (define-key paredit-mode-map (kbd "C-S-<right>") 'paredit-forward-slurp-sexp)
-			       (define-key paredit-mode-map (kbd "C-S-<left>") 'paredit-forward-barf-sexp)))
