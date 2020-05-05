5781ce09c577ebd7defb419f8cf48f9aba37c1fd
Mirko Hernandez <mirkoh@fastmail.com>
2020-05-04 22:46:53 -0400

    Multiple changes: - Include config for dabbrev, dired, ibuffer,magit. - Include hydra functions navigation, ibuffer. - Disable ido. - Update ivy config. - Enable electric-pair-mode (instead of smartparens-mode).

--- lisp/modes-config.el
+++ lisp/modes-config.el
@@ -1,13 +1,42 @@
+;;@============================= DABBREV
+;; (setq dabbrev-case-replace t)
+;; (setq dabbrev-case-fold-search t)
+;; (setq dabbrev-upcase-means-case-search t)
+
+;;@============================= DIRED
+
+(add-hook 'dired-mode-hook (lambda ()
+			     (put 'dired-find-alternate-file 'disabled nil)
+			     (dired-hide-details-mode)
+			     (dired-sort-toggle-or-edit)
+			     (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file) 
+			     (define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file "..")))
+			     ))
+
 ;;@============================= PROG-MODE
 (require 'smartparens-config)
 (add-hook 'prog-mode-hook (lambda ()
-			    (smartparens-mode)
-			    (toggle-input-method)))
+			    ;; (smartparens-mode)
+			    (electric-pair-mode)
+			    ;; (toggle-input-method)
+			    ))
 
 ;;@============================= MINIBUFFER
-(add-hook 'minibuffer-inactive-mode-hook (lambda ()
+;; M-x
+(add-hook 'minibuffer-setup-hook (lambda ()
 					   ;; (toggle-input-method)
 					   ))
+(add-hook 'helm-minor-mode-hook (lambda ()
+					   ;; (toggle-input-method)
+					   ))
+
+(add-hook 'helm-minor-mode-hook (lambda ()
+					   ;; (toggle-input-method)
+					   ))
+
+
+;;@============================= IBUFFER
+(setq ibuffer-default-sorting-mode 'major-mode)
 
 ;;@============================= IDO-UBIQUITOUS
 ;; (ido-ubiquitous-mode 1)
@@ -23,12 +52,18 @@
 (ivy-mode)
 
 (setq ivy-use-virtual-buffers nil) 
-(setq enable-recursive-minibuffers nil)
+(setq enable-recursive-minibuffers t)
 (setq ivy-count-format "(%d/%d) ")
 (setq ivy-height 15)
 ;; No regex 
-(setq ivy-initial-inputs-alist nil)
+;; (setq ivy-initial-inputs-alist nil)
 
+(setq ivy-re-builders-alist
+      '((swiper . ivy--regex-plus)
+	(swiper-isearch . ivy--regex-plus)
+        (t      . ivy--regex-fuzzy)))
+
+(setq ivy-display-style 'fancy)
 
 (ivy-set-actions
  'counsel-find-file
@@ -108,6 +143,7 @@
 				  (global-auto-complete-mode t)
 				  (auto-complete-mode t)
 				  (outline-minor-mode)
+				  (rainbow-mode)
 				  (setq outline-regexp ";;@")
 				  (diff-hl-mode t)))
 (add-hook 'slime-mode-hook 'set-up-slime-ac)
@@ -197,49 +233,8 @@
 
 
 ;;@============================= ORG
-(setq org-directory (concat emacs-root "emacs/org"))
 (setq org-modules '(org-bbdb org-bibtex org-docview org-gnus org-habit org-drill org-info org-irc org-mhe org-rmail org-w3m))
-
 (eval-after-load "org" '(add-to-list 'org-modules 'org-timer))
-
-(setq org-default-notes-file (concat org-directory "/notes.org"))
-(setq org-agenda-files (list  (concat org-directory)))
-
-(setq org-capture-templates
-      '(
-	("n"
-	 "Note"
-	 entry
-	 (file+headline (concat org-directory "/notes.org") "Notes")
-	 "*  %?\n")
-	("t"
-	 "Daily task"
-	 entry
-	 (file+headline (concat org-directory "/daily-tasks.org") "Tasks")
-	 "* TODO %?\n %U\n")
-	("p"
-	 "Practice"
-	 entry
-	 (file+headline (concat org-directory "/practice.org") "Practice")
-	 "\n\n* ZERO %? :practice:\nSCHEDULED:<%(org-read-date nil nil \"+1d\")  +1d>\n:PROPERTIES:\n:STYLE: habit\n:LOGGING: 3TIMES(!) 6TIMES(!) 10TIMES(!) FINISHED(!)\n:END:")
-	("7"
-	 "Practice a few days during the week."
-	 entry
-	 (file+headline (concat org-directory "/practice.org") "Practice")
-	 "\n\n* TODO  %? :practice:\nSCHEDULED:<%(org-read-date nil nil \"+1d\")  .+2d/3d>")
-	("j"
-	 "Journal"
-	 entry
-	 (file+olp+datetree  (concat org-directory "/journal.org"))
-	 "* %?\nEntered on %U\n ")))
-
-(setq org-agenda-custom-commands
-      '(("p" "Practice"
-	 ((agenda ""))
-	 ((org-agenda-show-log t)
-	  (org-agenda-ndays 7)
-	  (org-agenda-log-mode-items '(state))
-	  (org-agenda-skip-function '(org-agenda-skip-entry-if 'notregex ":practice:"))))))
 
 (setq org-todo-keywords ;; with utf8 characters.
       '((sequence "☛ TODO" "|" "✔ DONE")
@@ -252,12 +247,14 @@
 (add-hook 'org-mode-hook (lambda ()
 			   (setq default-justification 'full)
 			   (turn-on-auto-fill)
-			   (smartparens-mode)
-			   (toggle-input-method)
+			   ;; (smartparens-mode)
+			   (electric-pair-mode)
+			   ;; (toggle-input-method)
 			   ;; LOOKS
-			   (set-face-attribute 'org-level-1 t :height 1.9 )
-			   (set-face-attribute 'org-level-2 t :height 1.3 )
+			   ;; (set-face-attribute 'org-level-1 t :height 1.9 )
+			   ;; (set-face-attribute 'org-level-2 t :height 1.3 )
 			   (org-bullets-mode 1)))
+
 ;; LOOKS
 (set-face-attribute 'bold nil :height 130 :foreground "deep sky blue")
 
@@ -295,6 +292,8 @@
                                        "\\*Gofmt Errors\\*" "\\*autopep8"
                                        "\\*magit-process:" "\\*magit-diff:" "\\*anaconda-mode\\*"))
 
+
+
 ;;@============================= HYDRA
 (defvar rectangle-mark-mode)
 (defun hydra-ex-point-mark ()
@@ -313,14 +312,15 @@
 _m_ Set Bookmark                        _j_ Jump to Register         _i_ Insert Register
 _w_ Window Configuration to Register    _b_ Jump to Bookmark 
 _x_ Copy to Register
+_r_ Point to Register
 "
   ("b" counsel-bookmark)
   ("i" insert-register)
   ("j" jump-to-register)
+  ("r" point-to-register)
   ("m" bookmark-set)
   ("w" window-configuration-to-register)
   ("x" copy-to-register))
-
 
 (defhydra hydra-replace (:color orange)
   "Replace"
@@ -331,7 +331,6 @@ _x_ Copy to Register
   ("," smartscan-symbol-replace "Replace symbol in buffer")
   ("b" query-replace-regexp "Replace regexp in buffer or region")
   ("q" nil "cancel"))
-
 
 (defhydra hydra-rectangle (:body-pre (rectangle-mark-mode 1)
 				     :color pink
@@ -352,7 +351,138 @@ _x_ Copy to Register
   ("s" string-rectangle "string-rectangle")
   ("x" kill-rectangle "kill-rectangle")
   ("o" nil "" ))
+ 
+(defhydra hydra-navigation (:pre (set-cursor-color "#40e0d0")
+				 :post (progn
+					 (message
+					  "NAVIGATION MODE" )))
+  "navigation"
+  ("l" avy-goto-word-1)  
+  ("h" left-word)
+  ("J" next-line)
+  ("K" previous-line)
+  ("k" backward-paragraph)
+  ("j" forward-paragraph)
+  ("m" mark-sexp)
+  ("p" beginning-of-defun)
+  ("n" end-of-defun)
+  ("d" kill-sexp)
+  ("D" sp-kill-hybrid-sexp)
+  ("s" avy-goto-word-or-subword-1)
 
+  ("u" backward-up-list)
+  ("U" down-list)
 
+  ("g" beginning-of-buffer)
+  ("G" end-of-buffer)
+  
+  ("C-h" hs-hide-level)
+  ("C-l" recenter-top-bottom)
+  
+  ("C-j" my-hs-toggle-hiding)
+  ("5" paredit-open-round)
+  ("a" crux-move-beginning-of-line)
+  ("e" move-end-of-line)
+  ("i" helm-imenu)
+  ("SPC" nil "quit"))
+
+(defhydra hydra-ibuffer-main (:color pink :hint nil)
+  "
+ ^Navigation^ | ^Mark^        | ^Actions^        | ^View^
+-^----------^-+-^----^--------+-^-------^--------+-^----^-------
+  _k_:    ʌ   | _m_: mark     | _D_: delete      | _g_: refresh
+ _RET_: visit | _u_: unmark   | _S_: save        | _s_: sort
+  _j_:    v   | _*_: specific | _a_: all actions | _/_: filter
+-^----------^-+-^----^--------+-^-------^--------+-^----^-------
+"
+  ("j" ibuffer-forward-line)
+  ("RET" ibuffer-visit-buffer :color blue)
+  ("k" ibuffer-backward-line)
+
+  ("m" ibuffer-mark-forward)
+  ("u" ibuffer-unmark-forward)
+  ("*" hydra-ibuffer-mark/body :color blue)
+
+  ("D" ibuffer-do-delete)
+  ("S" ibuffer-do-save)
+  ("a" hydra-ibuffer-action/body :color blue)
+
+  ("g" ibuffer-update)
+  ("s" hydra-ibuffer-sort/body :color blue)
+  ("/" hydra-ibuffer-filter/body :color blue)
+
+  ("o" ibuffer-visit-buffer-other-window "other window" :color blue)
+  ("q" quit-window "quit ibuffer" :color blue)
+  ("." nil "toggle hydra" :color blue))
+
+(defhydra hydra-ibuffer-mark (:color teal :columns 5
+                              :after-exit (hydra-ibuffer-main/body))
+  "Mark"
+  ("*" ibuffer-unmark-all "unmark all")
+  ("M" ibuffer-mark-by-mode "mode")
+  ("m" ibuffer-mark-modified-buffers "modified")
+  ("u" ibuffer-mark-unsaved-buffers "unsaved")
+  ("s" ibuffer-mark-special-buffers "special")
+  ("r" ibuffer-mark-read-only-buffers "read-only")
+  ("/" ibuffer-mark-dired-buffers "dired")
+  ("e" ibuffer-mark-dissociated-buffers "dissociated")
+  ("h" ibuffer-mark-help-buffers "help")
+  ("z" ibuffer-mark-compressed-file-buffers "compressed")
+  ("b" hydra-ibuffer-main/body "back" :color blue))
+
+(defhydra hydra-ibuffer-action (:color teal :columns 4
+                                :after-exit
+                                (if (eq major-mode 'ibuffer-mode)
+                                    (hydra-ibuffer-main/body)))
+  "Action"
+  ("A" ibuffer-do-view "view")
+  ("E" ibuffer-do-eval "eval")
+  ("F" ibuffer-do-shell-command-file "shell-command-file")
+  ("I" ibuffer-do-query-replace-regexp "query-replace-regexp")
+  ("H" ibuffer-do-view-other-frame "view-other-frame")
+  ("N" ibuffer-do-shell-command-pipe-replace "shell-cmd-pipe-replace")
+  ("M" ibuffer-do-toggle-modified "toggle-modified")
+  ("O" ibuffer-do-occur "occur")
+  ("P" ibuffer-do-print "print")
+  ("Q" ibuffer-do-query-replace "query-replace")
+  ("R" ibuffer-do-rename-uniquely "rename-uniquely")
+  ("T" ibuffer-do-toggle-read-only "toggle-read-only")
+  ("U" ibuffer-do-replace-regexp "replace-regexp")
+  ("V" ibuffer-do-revert "revert")
+  ("W" ibuffer-do-view-and-eval "view-and-eval")
+  ("X" ibuffer-do-shell-command-pipe "shell-command-pipe")
+  ("b" nil "back"))
+
+(defhydra hydra-ibuffer-sort (:color amaranth :columns 3)
+  "Sort"
+  ("i" ibuffer-invert-sorting "invert")
+  ("a" ibuffer-do-sort-by-alphabetic "alphabetic")
+  ("v" ibuffer-do-sort-by-recency "recently used")
+  ("s" ibuffer-do-sort-by-size "size")
+  ("f" ibuffer-do-sort-by-filename/process "filename")
+  ("m" ibuffer-do-sort-by-major-mode "mode")
+  ("b" hydra-ibuffer-main/body "back" :color blue))
+
+(defhydra hydra-ibuffer-filter (:color amaranth :columns 4)
+  "Filter"
+  ("m" ibuffer-filter-by-used-mode "mode")
+  ("M" ibuffer-filter-by-derived-mode "derived mode")
+  ("n" ibuffer-filter-by-name "name")
+  ("c" ibuffer-filter-by-content "content")
+  ("e" ibuffer-filter-by-predicate "predicate")
+  ("f" ibuffer-filter-by-filename "filename")
+  (">" ibuffer-filter-by-size-gt "size")
+  ("<" ibuffer-filter-by-size-lt "size")
+  ("/" ibuffer-filter-disable "disable")
+  ("b" hydra-ibuffer-main/body "back" :color blue))
+
+(add-hook 'ibuffer-hook #'hydra-ibuffer-main/body)
 ;;@============================= XREF
 (add-to-list 'xref-backend-functions 'gxref-xref-backend)
+;;@============================= MAGIT
+(setq vc-handled-backends nil)
+(custom-theme-set-faces 'user
+			`(magit-diff-file-heading-highlight ((t (:foreground "lightskyblue"  :weight bold))))
+			`(magit-diff-file-heading ((t (:foreground "lightskyblue" ))))
+			)
+
